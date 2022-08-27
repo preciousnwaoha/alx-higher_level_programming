f62c3d9fc92cd8314f48507cd8fc20257e951dc9
Precious Nwaoha <nwaohaprecious25@gmail.com>
2022-08-27 20:09:07 +0000

    Made corrections on 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is a interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[38:66] + str[107:112] + str[:6]
+str = str[38:66] + str[106:111] + str[:7]
 print(str)
