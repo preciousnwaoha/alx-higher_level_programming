68a7ba17e9a895bf1a05900c73cba6213fe58063
Precious Nwaoha <nwaohaprecious25@gmail.com>
2022-08-28 04:55:25 +0000

    Mistake in Task 1 Fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 if number < 0:
     last_digit = -last_digit
-ans = "Last digit of {} is {}".format(number, last_digit)
+ans = "Last digit of {} is {} and is "format(number, last_digit)
 if last_digit > 5:
     ans += "greater than 5"
 elif last_digit == 0:
