4acd743127688ccfeff3d5c0d6d62daf6cbe29dd
Precious Nwaoha <nwaohaprecious25@gmail.com>
2022-09-08 01:58:02 +0000

    Second correction on 0x02

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Print a from file"""
+    """Print tjhe value of variable a variable_load_5."""
     from variable_load_5 import a
 
     print(a)
